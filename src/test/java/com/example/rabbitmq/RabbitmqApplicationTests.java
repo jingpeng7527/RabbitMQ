54bc5c5526231eb2b563455487bcb10c2c57e1d7
Jing Peng <114365503+jingpeng7527@users.noreply.github.com>
2025-11-02 19:25:43 -0800

    Change to local rabbitmq

--- src/test/java/com/example/rabbitmq/RabbitmqApplicationTests.java
+++ src/test/java/com/example/rabbitmq/RabbitmqApplicationTests.java
@@ -3,7 +3,10 @@ package com.example.rabbitmq;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.rabbitmq.listener.simple.auto-startup=false",
+		"spring.rabbitmq.ssl.enabled=false"
+})
 class RabbitmqApplicationTests {
 
 	@Test
